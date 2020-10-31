f01bcdbf23c5b0eacfa5b0cf78a84eb355b3c628
thisishoon <jihoon522@naver.com>
2020-10-31 23:28:57 +0900

    1. ES pk를 id값으로 설정하고, 중복이 일어나지 않도록 변경. 2. 단일 CRUD와 BULK CRUD를 위한 함수 개발(사용자 입장에서 통일된 방법 추구)

--- api2/api2/manager/views.py
+++ api2/api2/manager/views.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime, timedelta
 from dateutil.parser import parse, isoparse
 
-from elasticsearch import Elasticsearch, ElasticsearchException, helpers
+from elasticsearch import Elasticsearch, ElasticsearchException, helpers, NotFoundError
 
 
 # Create your views here.
@@ -54,10 +54,8 @@ class ManagerView(APIView):
                             content_type='application/json; charset=utf8')
 
     def post(self, request):
-
         data = json.loads(request.body)
         result = []
-
         if type(data) is not list:
             try:
                 data = json.loads(data)
@@ -66,38 +64,48 @@ class ManagerView(APIView):
 
         try:
             if type(data) is dict:
-                res = self.es.index(index='earthquake', body=data, id=data['id'])
-                # result.append(res['result'])
-            elif len(data) <= 1:
-                res = self.es.index(index='earthquake', body=data[0], id=data[0]['id'])
-                # result.append(res['result'])
+                data = [data]
             else:
                 for doc in data:
                     res = self.es.index(index='earthquake', body=doc, id=doc['id'])
-                    # result.append(res['result'])
-
+                    result.append(res['result'])
 
         except ElasticsearchException as e:
             print(e)
             return HttpResponse(status=400)
 
+        print(result)
+
         return HttpResponse("POST OK")
 
-    def put(self, request):
+    def put(self, request, pk=None):
         data = json.loads(request.body)
+        if type(data) is dict:
+            data = [data]
+
         for doc in data:
-            updated_id = doc['id']
-            print(updated_id)
-            
+            if pk is not None:
+                updated_id = pk
+            else:
+                updated_id = doc['id']
+
             self.es.update(index=self.index, id=updated_id,
                            body={"doc": doc})
 
         return HttpResponse("UPDATE OK")
 
-    def delete(self, pk):
-        deleted_id = pk.split('-')
-        print(deleted_id)
-        for id in deleted_id:
-            self.es.delete(index=self.index, id=id)
+    def delete(self, request, pk=None):
+        if pk is not None:
+            self.es.delete(index=self.index, id=pk)
+            return HttpResponse("DELETE OK")
+
+        data = json.loads(request.body)
+        print(data)
+        try:
+            for doc in data:
+                deleted_id = doc['id']
+                self.es.delete(index=self.index, id=deleted_id)
+        except NotFoundError:
+            pass
 
         return HttpResponse("DELETE OK")
